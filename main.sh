32d7a75c942186ee089194d04b4b49fb0e74cbba
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-08 20:24:06 +0300

    fix tzdata install

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+DEBIAN_FRONTEND=noninteractive
+
 # Add dependent repositories
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
 add-apt-repository https://ppa.pika-os.com
@@ -7,6 +9,8 @@ add-apt-repository ppa:kubuntu-ppa/backports
 cd ./python-apt
 
 # Get build deps
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
