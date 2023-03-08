afcffc3d2a1c53f2ccb5b5cce082f377cfdd86a2
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-08 20:48:57 +0300

    install build-deps on i386 #@

--- main32.sh
+++ main32.sh
@@ -8,10 +8,12 @@ add-apt-repository ppa:kubuntu-ppa/backports
 cd ./python-apt
 
 # Get build deps
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get install build-essential -y
 apt-get install crossbuild-essential-i386 lib32gcc-11-dev -y
 apt-get build-dep ./ -y -a i386
-apt-get install apt apt-utils python3-all  python3-all-dev python3-all-dbg python3-sphinx -y
+apt-get install apt apt-utils python3-all  python3-all-dev python3-all-dbg python3-sphinx apt  apt-utils dh-python libapt-pkg-dev python3-all python3-all-dev python3-all-dbg python3-distutils python3-distutils-extra python3-setuptools python3-sphinx pycodestyle pyflakes3 -y
 # Build package
 dpkg-buildpackage -a i386
 
